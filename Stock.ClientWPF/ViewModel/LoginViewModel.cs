8e5969f93d696a05b8962c5bfae287850c208fd8
karmazin.i.m <karmazin.i.m@gmail.com>
2020-02-09 23:28:33 +0200

    получил токен в ответ

--- Stock.ClientWPF/ViewModel/LoginViewModel.cs
+++ Stock.ClientWPF/ViewModel/LoginViewModel.cs
@@ -14,13 +14,17 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Navigation;
+using Newtonsoft.Json;
+using System.Web;
+using System.Net;
+using System.IO;
 
 namespace Stock.ClientWPF.ViewModel
 {
     class LoginViewModel : BaseViewModel
     {
-        private String login;
-        private String password;
+        private String login = "illya_ua";
+        private String password = "qwerty";
 
         public String Login
         {
@@ -51,20 +55,15 @@ namespace Stock.ClientWPF.ViewModel
         {
             get
             {
-                return goToHomePgeCommand ?? new RelayCommand<INotifyPropertyChanged>((INotifyPropertyChanged) =>
+                return goToHomePgeCommand ?? new RelayCommand<INotifyPropertyChanged>(async (INotifyPropertyChanged) =>
                 {
                     LoginModel loginModel = new LoginModel() { Username = Login, Password = Password };
                     HttpClient client = new HttpClient();
 
-                    
-                    var json = serializer.Serialize(new { id = 123, sum = new { amount = 0, currency = 643 } });
-                    var response = request.Post(address, json, contentType);
 
-                    using (var stream = client.GetAsync("http://localhost:20895/api/authorization"))
-                    {
-                        String html = stream.Result.Content.ReadAsStringAsync().Result;
-                        MessageBox.Show(html+ $"{loginModel.Username} {loginModel.Password}");
-                    }
+                    String json = JsonConvert.SerializeObject(loginModel, Formatting.Indented);
+
+                    await PostRequestAsync(loginModel);
 
                     Navigation.Navigate(Navigation.HomePageAlias, HomePageViewModel);
                 });
@@ -89,8 +88,38 @@ namespace Stock.ClientWPF.ViewModel
         //{
         //    GoToHomePgeCommand = new RelayCommand<INotifyPropertyChanged>((INotifyPropertyChanged) =>
         //    {
-               
+
         //    });
         //}
+
+        private static async Task PostRequestAsync(LoginModel loginModel)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:20895/api/authorization");
+            request.Method = "POST"; // для отправки используется метод Post
+                                     // данные для отправки
+            string data = JsonConvert.SerializeObject(loginModel, Formatting.Indented);
+            // преобразуем данные в массив байтов
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+            // устанавливаем тип содержимого - параметр ContentType
+            request.ContentType = "application/json";
+            // Устанавливаем заголовок Content-Length запроса - свойство ContentLength
+            request.ContentLength = byteArray.Length;
+
+            //записываем данные в поток запроса
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    MessageBox.Show(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+        }
     }
 }
